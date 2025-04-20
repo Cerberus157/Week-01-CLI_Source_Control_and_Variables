1a804bbc4efe6a343d5665174aa1690ca6004a0c
Cerberus157 <nyblood77@outlook.com>
2025-04-19 20:20:39 -0700

    First Commit

---
+++ Application.java
@@ -0,0 +1,11 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		int number1 = 6;
+	       int number2 = 4;
+	       System.out.println(number1 % number2);
+	}
+
+}
